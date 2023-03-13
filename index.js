8816e86400c6d1513c78ceb87c2f1183f8cdc0e7
Kucman <kucera.j@atlas.cz>
2023-03-13 23:08:57 +0000

    created a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Welcome to the calculator!');
+const readline = require('readline-sync');
+
+console.log('Please enter your first number:');
+const firstNumber = readline.prompt();
+var firstNumberFloat = parseFloat(firstNumber);
+
+console.log('Please enter your second number:');
+const secondNumber = readline.prompt();
+var secondNumberFloat = parseFloat(secondNumber);
+
+const result = secondNumberFloat * firstNumberFloat;
+console.log("The result is: " + result);
