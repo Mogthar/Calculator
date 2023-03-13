492cf0402478c43f622a4ee78eb6f74123435a54
Kucman <kucera.j@atlas.cz>
2023-03-13 23:23:59 +0000

    improved the calculator such that it only accepts numerical entries

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const readline = require('readline-sync');
 
 console.log('Please enter your first number:');
-const firstNumber = readline.prompt();
-var firstNumberFloat = parseFloat(firstNumber);
+var entrySuccessful = false;
+while(!entrySuccessful)
+{
+    const firstNumber = readline.prompt();
+    var firstNumberFloat = parseFloat(firstNumber);
+    if(!isNaN(firstNumberFloat))
+    {
+        entrySuccessful = true;
+    }
+    else
+    {
+        console.log("wrong entry format")
+    }
+}
 
+entrySuccessful = false;
 console.log('Please enter your second number:');
-const secondNumber = readline.prompt();
-var secondNumberFloat = parseFloat(secondNumber);
+while(!entrySuccessful)
+{
+    const secondNumber = readline.prompt();
+    var secondNumberFloat = parseFloat(secondNumber);
+    if(!isNaN(secondNumberFloat))
+    {
+        entrySuccessful = true;
+    }
+    else
+    {
+        console.log("wrong entry format")
+    }
+}
+
 
 const result = secondNumberFloat * firstNumberFloat;
 console.log("The result is: " + result);
