b16e8892b46cd5e1cca70d033f18a39b59cc2104
Kucman <kucera.j@atlas.cz>
2023-03-18 22:27:39 +0000

    implemented different operators

--- index.js
+++ index.js
@@ -1,54 +1,44 @@
 const readline = require('readline-sync');
+const allowedOperators = ['+', '-', '*', '/'];
+
+console.log('Please chose the mathematical operation')
+const operator = RequestOperator();
 
 console.log('Please enter your first number:');
-var entrySuccessful = false;
-while(!entrySuccessful)
-{
-    const firstNumber = readline.prompt();
-    var firstNumberFloat = parseFloat(firstNumber);
-    if(!isNaN(firstNumberFloat))
-    {
-        entrySuccessful = true;
-    }
-    else
-    {
-        console.log("wrong entry format")
-    }
-}
+const firstNumber = RequestNumber();
 
-entrySuccessful = false;
 console.log('Please enter your second number:');
-while(!entrySuccessful)
-{
-    const secondNumber = readline.prompt();
-    var secondNumberFloat = parseFloat(secondNumber);
-    if(!isNaN(secondNumberFloat))
-    {
-        entrySuccessful = true;
-    }
-    else
-    {
-        console.log("wrong entry format")
-    }
+const secondNumber = RequestNumber()
+
+var result;
+switch(operator){
+    case '+':
+        result = firstNumber + secondNumber;
+        break;
+    case '-':
+        result = firstNumber - secondNumber;
+        break;
+    case '*':
+        result = firstNumber * secondNumber;
+        break;
+    case '/':
+        result = firstNumber / secondNumber;
+        break;
 }
-
-
-const result = secondNumberFloat * firstNumberFloat;
 console.log("The result is: " + result);
 
 
 // create a function called request a number which returns the requested number
-/*
 function RequestNumber()
 {
     entrySuccessful = false;
     while(!entrySuccessful)
     {
-        const secondNumber = readline.prompt();
-        var secondNumberFloat = parseFloat(secondNumber);
-        if(!isNaN(secondNumberFloat))
+        let entry = readline.prompt();
+        let number = parseFloat(entry);
+        if(!isNaN(number))
         {
-            return secondNumberFloat
+            return number
         }
         else
         {
@@ -57,4 +47,20 @@ function RequestNumber()
     }
 
 }
-*/
+
+function RequestOperator()
+{
+    entrySuccessful = false;
+    while(!entrySuccessful)
+    {
+        let entry = readline.prompt();
+        if(allowedOperators.includes(entry))
+        {
+            return entry
+        }
+        else
+        {
+            console.log("wrong entry format")
+        }
+    }
+}
